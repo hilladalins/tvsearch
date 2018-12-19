c4510faec84496b40c4d6d94b45bfd95ebe25343
Tal_Kleiner <talkl120@gmail.com>
2018-12-19 16:36:45 +0200

    Try to commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from bottle import (get, post, redirect, request, route, run, static_file,
                     template)
 import utils
+import json
+
 
 # Static Routes
 
@@ -9,19 +11,28 @@ import utils
 def js(filepath):
     return static_file(filepath, root="./js")
 
+
 @get("/css/<filepath:re:.*\.css>")
 def css(filepath):
     return static_file(filepath, root="./css")
+
 
 @get("/images/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
 def img(filepath):
     return static_file(filepath, root="./images")
 
+
 @route('/')
 def index():
     sectionTemplate = "./templates/home.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+
+
+@route('/browse')
+def index():
+    sectionTemplate = "./templates/browse.tpl"
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate,
+                    sectionData={utils.all_shows})
+
 
 run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
-
-print('test')
--- utils.py
+++ utils.py
@@ -2,10 +2,12 @@ from bottle import template
 import json
 
 JSON_FOLDER = './data'
-AVAILABE_SHOWS = ["7", "66", "73", "82", "112", "143", "175", "216", "1371", "1871","2993", "305"]
+AVAILABLE_SHOWS = ["7", "66", "73", "82", "112", "143", "175", "216", "1371", "1871", "2993", "305"]
+
 
 def getVersion():
     return "0.0.1"
+
 
 def getJsonFromFile(showName):
     try:
