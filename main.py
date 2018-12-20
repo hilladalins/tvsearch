8e9bc4bb30ccc47b26aa67cc77ecf469351f5c83
hilla dalins <hilladalins@gmail.com>
2018-12-20 15:42:57 +0200

    implemented route for sorting the shows

--- main.py
+++ main.py
@@ -29,12 +29,28 @@ def index():
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
 
 
-@route('/browse')
-def browse():
+@route('/<path:re:browse(/name|/ratings)?>')
+def browse(path):
     sectionTemplate = "./templates/browse.tpl"
-    sectionData = utils.get_all_shows_sorted('ratings')
+    if path == 'browse/name':
+        sectionData = utils.get_all_shows_sorted('name')
+    elif path == 'browse/ratings':
+        sectionData = utils.get_all_shows_sorted('ratings')
+    else:
+        sectionData = utils.getAllShows()
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate,
                     sectionData=sectionData)
+
+#
+# @route('/browse/<sort:re.*\>')
+# def browse(sort):
+#     sectionTemplate = "./templates/browse.tpl"
+#     if sort:
+#         sectionData = utils.get_all_shows_sorted(sort)
+#     else:
+#         sectionData = utils.getAllShows()
+#     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate,
+#                     sectionData=sectionData)
 
 
 # to be implemented in order to bake a parameter to the browse function
